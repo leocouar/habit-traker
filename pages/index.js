be1bf500b58963a7f5d19a579292075fc3ced280
leonardo <leonardo.couar@gmail.com>
2023-11-23 08:45:51 -0300

    eliminar boton inicio

--- pages/index.js
+++ pages/index.js
@@ -16,9 +16,6 @@ export default function Home() {
 
             <div className="text-center">
               <h1 className="text-4xl font-bold mb-4">Bienvenido</h1>
-              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                Iniciar
-              </button>
             </div>
 
           </section>
